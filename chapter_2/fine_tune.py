4dff41e698cd6959dfaf09f049b5ecec9970faf7
Yan Drugalya <yan@unity3d.com>
2023-07-25 21:36:14 +0200

    Add possibility to specify model name

--- chapter_2/fine_tune.py
+++ chapter_2/fine_tune.py
@@ -5,6 +5,13 @@ import matplotlib.pyplot as plt
 
 DEFAULT_FINE_TUNE_ITER = 4
 
+MODEL_NAME_TO_FUNCTION = {
+    "resnet34": resnet34,
+    "resnet50": resnet50,
+    "resnet101": resnet101,
+    "resnet152": resnet152,
+}
+
 def create_data_block():
     return DataBlock(
         blocks=(ImageBlock, CategoryBlock), 
@@ -15,8 +22,12 @@ def create_data_block():
         batch_tfms=aug_transforms(mult=2)
     )
 
-def train_model(dls, fine_tune_iter):
-    learn = vision_learner(dls, resnet152, metrics=error_rate)
+def train_model(dls, fine_tune_iter, model_name):
+    model_func = MODEL_NAME_TO_FUNCTION.get(model_name)
+    if model_func is None:
+        raise ValueError(f"Unsupported model name: {model_name}")
+    
+    learn = vision_learner(dls, model_func, metrics=error_rate)
     learn.fine_tune(fine_tune_iter)
     return learn
 
@@ -29,11 +40,12 @@ def interpret_model(learn):
 def save_model(learn, model_file_path):
     learn.export(model_file_path)
 
-def fine_tune_model(data_dir, save_model_as, force, fine_tune_iter):
+def fine_tune_model(data_dir, save_model_as, force, fine_tune_iter, model_name):
     print(f"Data directory: {data_dir}")
     print(f"Model will be saved as: {save_model_as}")
     print(f"Force re-training: {force}")
     print(f"Fine tune iterations: {fine_tune_iter}")
+    print(f"Model name: {model_name}")
     # Skip training if the model already exists and force is not specified
     if not force and Path(save_model_as).exists():
         print(f"Model {save_model_as} already exists. Use --force to retrain.")
@@ -47,7 +59,7 @@ def fine_tune_model(data_dir, save_model_as, force, fine_tune_iter):
     # Load data and train model
     dls = create_data_block().dataloaders(data_dir)
     print("Training model...")
-    learn = train_model(dls, fine_tune_iter)
+    learn = train_model(dls, fine_tune_iter, model_name)
 
     # Interpret and save model
     interpret_model(learn)
@@ -61,12 +73,13 @@ def parse_arguments():
     parser.add_argument("--save-model-as", type=str, required=True, help="Where to save model file.")
     parser.add_argument("--fine-tune-iter", type=int, default=DEFAULT_FINE_TUNE_ITER, help="Number of fine tuning iterations")
     parser.add_argument("--force", action="store_true", help="Force retraining even if model already exists.")
+    parser.add_argument("--model-name", type=str, default="resnet152", help="Model architecture name")
     return parser.parse_args()
 
 def main():
     """Main function to parse arguments and start model training"""
     args = parse_arguments()
-    fine_tune_model(args.data_dir, args.save_model_as, args.force, args.fine_tune_iter)
+    fine_tune_model(args.data_dir, args.save_model_as, args.force, args.fine_tune_iter, args.model_name)
     print(f"Done. Model saved as {args.save_model_as}")
 
 if __name__ == "__main__":
